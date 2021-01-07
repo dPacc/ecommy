bf3ffd7e11647e6344b6e782e7a264261842e5ad
dPacc <themdk01@gmail.com>
2021-01-07 09:31:57 +0400

    add logout, clear redux state

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -4,17 +4,34 @@ import {
   HomeOutlined,
   LoginOutlined,
   UserAddOutlined,
+  LogoutOutlined,
   ProfileOutlined,
 } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 
+import firebase from "firebase/app";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+
 const { SubMenu, Item } = Menu;
 
 const Header = () => {
+  const history = useHistory();
+  const dispatch = useDispatch();
   const [current, setCurrent] = useState("");
 
   const handleClick = (e) => {
     setCurrent(e.key);
+  };
+
+  const handleLogout = () => {
+    firebase.auth().signOut();
+    dispatch({
+      type: "LOGOUT",
+      payload: null,
+    });
+
+    history.push("/login");
   };
 
   return (
@@ -34,6 +51,9 @@ const Header = () => {
       <SubMenu key="SubMenu" icon={<ProfileOutlined />} title="Profile">
         <Item key="setting:1">Option 1</Item>
         <Item key="setting:2">Option 2</Item>
+        <Item icon={<LogoutOutlined />} key="logout" onClick={handleLogout}>
+          Logout
+        </Item>
       </SubMenu>
     </Menu>
   );
