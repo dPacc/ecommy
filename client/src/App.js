89c649afb7a93f04cd835597062d1f2bd8c3c668
dPacc <themdk01@gmail.com>
2021-01-07 09:14:54 +0400

    dispatch email, token to redux if user is authenticated

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -6,7 +6,32 @@ import "react-toastify/dist/ReactToastify.css";
 import { Login, Register, Home, RegisterComplete } from "./views";
 import { Header } from "./components";
 
+import { auth } from "./firebase";
+import { useDispatch } from "react-redux";
+
 const App = () => {
+  const dispatch = useDispatch();
+
+  // to check firebase auth state
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const idTokenResult = await user.getIdTokenResult();
+
+        dispatch({
+          type: "LOGGED_IN_USER",
+          payload: {
+            email: user.email,
+            token: idTokenResult.token,
+          },
+        });
+      }
+    });
+
+    // cleanup
+    return () => unsubscribe();
+  }, []);
+
   return (
     <>
       <Header />
