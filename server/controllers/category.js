0c586ee8fbb16e0e86eab37ca886a3d583db3536
dPacc <themdk01@gmail.com>
2021-01-08 16:38:07 +0400

    add category routes and controllers

---
+++ server/controllers/category.js
@@ -0,0 +1,17 @@
+const Category = require("../models/category");
+
+exports.create = (req, res) => {
+  //
+};
+exports.read = (req, res) => {
+  //
+};
+exports.update = (req, res) => {
+  //
+};
+exports.remove = (req, res) => {
+  //
+};
+exports.list = (req, res) => {
+  //
+};
---
+++ server/routes/category.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+
+// route middlewares
+const { authCheck, adminCheck } = require("../middlewares/auth");
+
+// controllers
+const {
+  create,
+  read,
+  update,
+  remove,
+  list,
+} = require("../controllers/category");
+
+// routes
+router.post("/category", authCheck, adminCheck, create);
+router.get("/categories", list);
+router.get("/category/:slug", authCheck, adminCheck, read);
+router.put("/category/:slug", authCheck, adminCheck, update);
+router.delete("/category/:slug", authCheck, adminCheck, remove);
+
+module.exports = router;
