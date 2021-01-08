20f3408aa91d4489ebd69b71556e2e78fecb5757
dPacc <themdk01@gmail.com>
2021-01-08 17:17:50 +0400

    add create category controller, change category name minlength to 2

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,17 +1,27 @@
 const Category = require("../models/category");
+const slugify = require("slugify");
 
-exports.create = (req, res) => {
+exports.create = async (req, res) => {
+  const { name } = req.body;
+
+  try {
+    const category = await new Category({ name, slug: slugify(name) }).save();
+    res.json(category);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("Create category failed");
+  }
   //
 };
-exports.read = (req, res) => {
+exports.read = async (req, res) => {
   //
 };
-exports.update = (req, res) => {
+exports.update = async (req, res) => {
   //
 };
-exports.remove = (req, res) => {
+exports.remove = async (req, res) => {
   //
 };
-exports.list = (req, res) => {
+exports.list = async (req, res) => {
   //
 };
