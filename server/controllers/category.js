854124df3643f59d819a777a66f7a4483a2e282d
dPacc <themdk01@gmail.com>
2021-01-08 18:10:31 +0400

    add list, read, update and delete operations for category

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -13,15 +13,55 @@ exports.create = async (req, res) => {
   }
   //
 };
-exports.read = async (req, res) => {
-  //
-};
-exports.update = async (req, res) => {
-  //
-};
-exports.remove = async (req, res) => {
-  //
-};
+
 exports.list = async (req, res) => {
-  //
+  try {
+    const categories = await Category.find({}).sort({ createdAt: -1 }).exec();
+    if (categories) {
+      res.json(categories);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(404).send("No categories found");
+  }
+};
+
+exports.read = async (req, res) => {
+  const { slug } = req.params;
+  const category = await Category.findOne({ slug }).exec();
+
+  if (category) {
+    res.json(category);
+  } else {
+    res.status(404).send("Category not found");
+  }
+};
+
+exports.update = async (req, res) => {
+  const { slug } = req.params;
+  const { name } = req.body;
+
+  try {
+    const categoryUpdated = await Category.findOneAndUpdate(
+      { slug },
+      { name, slug: slugify(name) },
+      { new: true }
+    ).exec();
+    res.json(categoryUpdated);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("Create update failed");
+  }
+};
+
+exports.remove = async (req, res) => {
+  const { slug } = req.params;
+
+  try {
+    const categoryDeleted = await Category.findOneAndDelete({ slug }).exec();
+    res.json(categoryDeleted);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("Create delete failed");
+  }
 };
