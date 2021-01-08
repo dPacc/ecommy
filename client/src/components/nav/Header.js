5013e6954d2db562ec565612e9776e07cfb86087
dPacc <themdk01@gmail.com>
2021-01-08 21:01:38 +0400

    dynamically render user and admin dashboard on navbar dropdown

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -59,8 +59,15 @@ const Header = () => {
           title={user.email && user.email.split("@")[0]}
           className="float-right"
         >
-          <Item key="setting:1">Option 1</Item>
-          <Item key="setting:2">Option 2</Item>
+          {user.role === "subscriber" ? (
+            <Item key="subscriber">
+              <Link to="/user/history">Dashboard</Link>
+            </Item>
+          ) : (
+            <Item key="admin">
+              <Link to="/admin/dashboard">Admin Dashboard</Link>
+            </Item>
+          )}
           <Item icon={<LogoutOutlined />} key="logout" onClick={handleLogout}>
             Logout
           </Item>
