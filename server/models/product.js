385a35b5e175b92acc65b9acdb5932f4ddcd6966
dPacc <themdk01@gmail.com>
2021-01-10 14:05:15 +0400

    fix: missin new keyword for schema

---
+++ server/models/product.js
@@ -0,0 +1,4 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const productSchema = new mongoose.Schema({});
--- server/models/subcategory.js
+++ server/models/subcategory.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Schema;
 
-const SubcategorySchema = mongoose.Schema(
+const SubcategorySchema = new mongoose.Schema(
   {
     name: {
       type: String,
