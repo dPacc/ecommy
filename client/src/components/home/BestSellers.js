c9906bcfe6a61ac0c40a758eaaff4c859d6dd514
dPacc <themdk01@gmail.com>
2021-01-20 19:27:39 +0400

    add pagination to BestSeller component

--- client/src/components/home/BestSellers.js
+++ client/src/components/home/BestSellers.js
@@ -1,19 +1,28 @@
 import React, { useState, useEffect } from "react";
-import { getProducts } from "../../api/product";
+import { getProducts, getProductsCount } from "../../api/product";
 import { ProductCard, LoadingCard } from "../../components";
+import { Pagination } from "antd";
 
 const BestSellers = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [productsCount, setProductsCount] = useState(0);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     loadAllProducts();
-  }, []);
+  }, [page]);
+
+  useEffect(() => {
+    getProductsCount().then((res) => {
+      setProductsCount(res.data);
+    });
+  });
 
   const loadAllProducts = () => {
     setLoading(true);
     // sort, order, limit
-    getProducts("sold", "desc", 1).then((res) => {
+    getProducts("sold", "desc", page).then((res) => {
       setProducts(res.data);
       setLoading(false);
     });
@@ -33,6 +42,16 @@ const BestSellers = () => {
             ))}
           </div>
         )}
+      </div>
+
+      <div className="row">
+        <nav className="col-md-4 offset-md-4 text-center pt-5 p-3">
+          <Pagination
+            current={page}
+            total={(productsCount / 3) * 10}
+            onChange={(val) => setPage(val)}
+          />
+        </nav>
       </div>
     </>
   );
