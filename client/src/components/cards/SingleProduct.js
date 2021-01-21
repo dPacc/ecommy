4379629d4e9850a5d7b67c8a23a6a18cbd3a32fe
dPacc <themdk01@gmail.com>
2021-01-21 22:35:31 +0400

    add product image carousel, modify css

--- client/src/components/cards/SingleProduct.js
+++ client/src/components/cards/SingleProduct.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Card } from "antd";
 import { Link } from "react-router-dom";
 import { HeartOutlined, ShoppingCartOutlined } from "@ant-design/icons";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
 const { Meta } = Card;
 
@@ -9,7 +11,12 @@ const SingleProduct = ({ product }) => {
   const { title, description, images, slug } = product;
   return (
     <>
-      <div className="col-md-7">Image</div>
+      <div className="col-md-7">
+        <Carousel showArrows={true} autoPlay infiniteLoop>
+          {images &&
+            images.map((i) => <img src={i.url} alt="img1" key={i.public_id} />)}
+        </Carousel>
+      </div>
       <div className="col-md-5">
         <Card
           actions={[
