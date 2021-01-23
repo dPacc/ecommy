d8689c1542351c0d29ed3c60242d269ced753ad1
dPacc <themdk01@gmail.com>
2021-01-23 20:34:07 +0400

    push user back to original location after logging in

--- client/src/components/modal/RatingModal.js
+++ client/src/components/modal/RatingModal.js
@@ -3,18 +3,22 @@ import { Modal } from "antd";
 import { toast } from "react-toastify";
 import { useSelector } from "react-redux";
 import { StarOutlined } from "@ant-design/icons";
-import { useHistory } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 
 const RatingModal = ({ children }) => {
   const { user } = useSelector((state) => ({ ...state }));
   const [modalVisible, setModalVisible] = useState(false);
   const history = useHistory();
+  const { slug } = useParams();
 
   const handleModal = () => {
     if (user && user.token) {
       setModalVisible(true);
     } else {
-      history.push("/login");
+      history.push({
+        pathname: "/login",
+        state: { from: `/product/${slug}` },
+      });
     }
   };
   return (
--- client/src/views/auth/Login.js
+++ client/src/views/auth/Login.js
@@ -15,16 +15,28 @@ const Login = ({ history }) => {
   const { user } = useSelector((state) => ({ ...state }));
 
   useEffect(() => {
-    if (user && user.token) {
-      history.push("/");
+    const intended = history.location.state;
+
+    if (intended) {
+      return;
+    } else {
+      if (user && user.token) {
+        history.push("/");
+      }
     }
   }, [history, user]);
 
   const roleBasedRedirect = (res) => {
-    if (res.data.role === "admin") {
-      history.push("/admin/dashboard");
+    const intended = history.location.state;
+
+    if (intended) {
+      history.push(intended.from);
     } else {
-      history.push("/user/history");
+      if (res.data.role === "admin") {
+        history.push("/admin/dashboard");
+      } else {
+        history.push("/user/history");
+      }
     }
   };
 
