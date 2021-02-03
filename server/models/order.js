8303d147bae146207abaf468c757cce958576232
dPacc <themdk01@gmail.com>
2021-02-03 15:39:47 +0400

    create order model for each purchase

---
+++ server/models/order.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const orderSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        product: {
+          type: ObjectId,
+          ref: "Product",
+        },
+        count: Number,
+        color: String,
+      },
+    ],
+    paymentIntent: {},
+    orderStatus: {
+      type: String,
+      default: "Not Processed",
+      enum: [
+        "Not Processed",
+        "Processing",
+        "Dispatched",
+        "Cancelled",
+        "Completed",
+      ],
+    },
+    orderedBy: {
+      type: ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Order", orderSchema);
