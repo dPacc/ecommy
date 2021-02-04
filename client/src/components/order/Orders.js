d9e135c5cb220d4d37fad9a7b9d5683c9124c3fb
dPacc <themdk01@gmail.com>
2021-02-04 21:02:23 +0400

    add products table below order status for admin

--- client/src/components/order/Orders.js
+++ client/src/components/order/Orders.js
@@ -1,8 +1,44 @@
 import React from "react";
-import { CheckCircledOutlined, CloseCircleOutlined } from "@ant-design/icons";
+import { CheckCircleOutlined, CloseCircleOutlined } from "@ant-design/icons";
 import { ShowPaymentInfo } from "../../components";
 
 const Orders = ({ orders, handleStatusChange }) => {
+  const showOrderInTable = (order) => (
+    <table className="table table-bordered">
+      <thead className="thead-light">
+        <tr>
+          <th scope="col">Title</th>
+          <th scope="col">Price</th>
+          <th scope="col">Brand</th>
+          <th scope="col">Color</th>
+          <th scope="col">Count</th>
+          <th scope="col">Shipping</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {order.products.map((p, i) => (
+          <tr key={i}>
+            <td>
+              <b>{p.product.title}</b>
+            </td>
+            <td> {p.product.price}</td>
+            <td> {p.product.brand}</td>
+            <td> {p.color}</td>
+            <td> {p.count}</td>
+            <td>
+              {" "}
+              {p.product.shipping === "Yes" ? (
+                <CheckCircleOutlined style={{ color: "green" }} />
+              ) : (
+                <CloseCircleOutlined style={{ color: "red" }} />
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
   return (
     <>
       {orders.map((order) => (
@@ -29,6 +65,8 @@ const Orders = ({ orders, handleStatusChange }) => {
               </div>
             </div>
           </div>
+
+          {showOrderInTable(order)}
         </div>
       ))}
     </>
