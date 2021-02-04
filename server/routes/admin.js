920c0e907045b9915f97dbf84bbaaeac0762304e
dPacc <themdk01@gmail.com>
2021-02-04 20:17:02 +0400

    add admin get all orders, update order status route and controllers

---
+++ server/routes/admin.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+// route middlewares
+const { authCheck, adminCheck } = require("../middlewares/auth");
+
+// controllers
+const { orders, orderStatus } = require("../controllers/admin");
+
+// routes
+router.get("/admin/orders", authCheck, adminCheck, orders);
+router.put("/admin/order-status", authCheck, adminCheck, orderStatus);
+
+module.exports = router;
