6296b9c275d4a21d088080e83c90b8770740ad07
dPacc <themdk01@gmail.com>
2021-02-05 19:13:15 +0400

    create cod reducer to set to true, if user clicks that option

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -4,6 +4,7 @@ import { searchReducer } from "./searchReducer";
 import { cartReducer } from "./cartReducer";
 import { drawerReducer } from "./drawerReducer";
 import { couponReducer } from "./couponReducer";
+import { codReducer } from "./codReducer";
 
 const rootReducer = combineReducers({
   user: userReducer,
@@ -11,6 +12,7 @@ const rootReducer = combineReducers({
   cart: cartReducer,
   drawer: drawerReducer,
   coupon: couponReducer,
+  cod: codReducer,
 });
 
 export default rootReducer;
